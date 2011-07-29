3e9e41efb338c1f7d83e381e54c0a2184c06d104
Johan Sageryd <j416@1616.se>
2011-07-29 23:33:36 +0200

    Quick Look for folders doesn't seem to work in Lion so remove it

--- fixfinder.bash
+++ fixfinder.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Finder
-
 # Use POSIX paths in title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-
-# Show folder contents in Quick Look
-defaults write com.apple.finder QLEnableXRayFolders 1
 
 killall Finder
--- restorefinder.bash
+++ restorefinder.bash
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Finder
-
 # Don't use POSIX paths in title
 defaults delete com.apple.finder _FXShowPosixPathInTitle
-
-# Don't show folder contents in Quick Look
-defaults delete com.apple.finder QLEnableXRayFolders
 
 killall Finder
